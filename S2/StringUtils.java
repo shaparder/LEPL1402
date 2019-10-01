e57f200676d4d185975e3f4cd2143d3e9257f84c
shaparder <shaparder.contact@gmail.com>
2019-10-02 01:21:51 +0200

    S2 StringUtils Began

--- S2/StringUtils.java
+++ S2/StringUtils.java
@@ -6,8 +6,32 @@ public class StringUtils {
      * for example split("test-test", '-') => {"test", "test"}
      * Must return null if there is no occurrence of 'marker' in 'str'
      */
-    public static String [] split(String str, char marker){
-
+    public static String[] split(String str, char marker){
+		String[] split;
+		int len = str.length();
+		int delim_count = 0;
+		int index = 0;
+		for (int i = 0; i < len; i++){
+			if (str.charAt(i) == marker && (i + 1 < len))
+				delim_count++;
+		}
+		if (delim_count == 0)
+			return null;
+		split = new String[delim_count + 1];
+		StringBuilder chunk = new StringBuilder(len - delim_count);
+		for (int j = 0; j < len; j++){
+			if (str.charAt(j) == marker){
+				split[index] = chunk.toString();
+				chunk = new StringBuilder(len - j);
+				index++;
+			} else {
+				chunk.append(str.charAt(j));
+			}
+		}
+		if (chunk.length() > 0) {
+			split[index] = chunk.toString();
+		}
+		return split;
     }
 
 
@@ -16,23 +40,31 @@ public class StringUtils {
      * or -1 if there is no occurrence of sub in str at all.
      * Be careful, we ask you to make CASE SENSITIVE comparison between str and sub.
      */
-    public static int indexOf(String str, String sub){
+    // public static int indexOf(String str, String sub){
 
-    }
+    // }
 
 
-    public static String toLowerCase(String str){
+    // public static String toLowerCase(String str){
 
-    }
+    // }
 
 
     /*
      * Returns true if the string 'str' is a palindrome (a string that reads the same from
      * left to right AND from right to left).
      */
-    public static boolean palindrome(String str){
-    
-    }
+    // public static boolean palindrome(String str){
 
+    // }
+
+	public static void main(String[] args) {
+		String str = "prout. i. got. it.";
+		char delim = '.';
+		String[] arrtest = split(str, delim);
+		for(String s: arrtest){
+			System.out.println(s);
+		}
+	}
 
 }
